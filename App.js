88abc8a9093005574bafe6f72e0b1c72b00b7330
Amol <mailmeamol2112@gmail.com>
2021-04-11 18:18:00 -0400

    converted App to arrow function

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import "h8k-components";
 
 const title = "Customer List";
 
-function App() {
+const App = () => {
   return (
     <div className="container">
       <h8k-navbar header={title} />
       <CustomerList />
     </div>
   );
-}
+};
 
 export default App;
